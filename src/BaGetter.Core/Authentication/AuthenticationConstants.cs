50e8448781b047d8ce0a09b0452d458044ce912b
Tomáš Filip <tomasfilip124@gmail.com>
2024-06-17 10:55:08 +0200

    Cleanup

--- src/BaGetter.Core/Authentication/AuthenticationConstants.cs
+++ src/BaGetter.Core/Authentication/AuthenticationConstants.cs
@@ -7,6 +7,6 @@ using System.Threading.Tasks;
 namespace BaGetter.Authentication;
 public static class AuthenticationConstants
 {
-    public const string NugetBasicAuthenticationScheme = "BasicAuthentication";
+    public const string NugetBasicAuthenticationScheme = "NugetBasicAuthentication";
     public const string NugetUserPolicy = "NuGetUserPolicy";
 }
